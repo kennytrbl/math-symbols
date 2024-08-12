3912df2c4c063727e4e305690de2a0bec90519e3
kennytrbl <kennyzhang418@gmail.com>
2024-08-12 00:39:15 -0700

    local push

--- src/App.tsx
+++ src/App.tsx
@@ -22,6 +22,84 @@ const App = () => {
       description: "approximation",
       example: "π ≈ 3.14159",
     },
+    {
+      symbol: ">",
+      name: "greater than",
+      description: "comparison",
+      example: "5 > 4",
+    },
+    {
+      symbol: "<",
+      name: "less than",
+      description: "comparison",
+      example: "4 < 5",
+    },
+    {
+      symbol: "≥",
+      name: "greater than or equal to",
+      description: "comparison",
+      example: "4 ≥ 4",
+    },
+    {
+      symbol: "≤",
+      name: "less than or equal to",
+      description: "comparison",
+      example: "4 ≤ 4",
+    },
+    {
+      symbol: "+",
+      name: "plus sign",
+      description: "addition",
+      example: "2 + 2 = 4",
+    },
+    {
+      symbol: "-",
+      name: "minus sign",
+      description: "subtraction",
+      example: "4 - 2 = 2",
+    },
+    {
+      symbol: "±",
+      name: "plus-minus sign",
+      description: "plus or minus",
+      example: "x = ±2",
+    },
+    {
+      symbol: "*",
+      name: "asterisk",
+      description: "multiplication",
+      example: "2 * 2 = 4",
+    },
+    {
+      symbol: "x",
+      name: "multiplication sign",
+      description: "multiplication",
+      example: "2 x 2 = 4",
+    },
+    {
+      symbol: "·",
+      name: "middle dot",
+      description: "multiplication",
+      example: "2 · 2 = 4",
+    },
+    {
+      symbol: "÷",
+      name: "division sign",
+      description: "division",
+      example: "4 ÷ 2 = 2",
+    },
+    {
+      symbol: "/",
+      name: "division sign",
+      description: "division",
+      example: "4 / 2 = 2",
+    },
+    {
+      symbol: "_",
+      name: "horizontal line",
+      description: "division",
+      example: "4 _ 2 = 2",
+    },
   ];
 
   return (
